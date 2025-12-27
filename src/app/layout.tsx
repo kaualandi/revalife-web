dda694daae86786f99c6161050f5a846ce5fd418
Kauã Landi <eu@kaualf.com>
2025-12-27 11:19:33 -0300

    fix: gtm script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import './globals.css';
 
+import { GoogleTagManager } from '@next/third-parties/google';
 import { QueryProvider } from '@/providers/query-provider';
 import { Archivo } from 'next/font/google';
 import type { Metadata } from 'next';
-import Script from 'next/script';
 
 const archivo = Archivo({
   variable: '--font-archivo',
@@ -23,15 +23,7 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <body className={`${archivo.variable} font-sans antialiased`}>
-        <Script
-          id="gtm-script"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s);j.async=true;j.src="https://load.stape.protocoloemagreser.com.br/d03kprnxtdki.js?"+i;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','az0y=AA9ZMywjXz0iIDAsIjkoVh9WVlpJSBYeVBsWGwwBER8FBAEZGQkAFRoOFlobAR9eCxk%3D');
-            `,
-          }}
-        />
+        <GoogleTagManager gtmId="GTM-KT4952TW" />
         <QueryProvider>{children}</QueryProvider>
       </body>
     </html>
