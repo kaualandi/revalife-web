515dbe474eb2a1f9a75ecbe48d39cc284c633db8
Kauã Landi <eu@kaualf.com>
2025-12-27 11:17:37 -0300

    feat: implement GTM with custom server

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css';
 import { QueryProvider } from '@/providers/query-provider';
 import { Archivo } from 'next/font/google';
 import type { Metadata } from 'next';
+import Script from 'next/script';
 
 const archivo = Archivo({
   variable: '--font-archivo',
@@ -22,6 +23,15 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <body className={`${archivo.variable} font-sans antialiased`}>
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s);j.async=true;j.src="https://load.stape.protocoloemagreser.com.br/d03kprnxtdki.js?"+i;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','az0y=AA9ZMywjXz0iIDAsIjkoVh9WVlpJSBYeVBsWGwwBER8FBAEZGQkAFRoOFlobAR9eCxk%3D');
+            `,
+          }}
+        />
         <QueryProvider>{children}</QueryProvider>
       </body>
     </html>
